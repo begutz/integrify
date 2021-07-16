e366ec50fe7ccf3c443b21e06b23301a0a79c0aa
Begum Utz <begum.utz@gmail.com>
2021-07-16 23:06:12 +0300

    add profilepic-letter

--- src/components/ProfilePic.js
+++ src/components/ProfilePic.js
@@ -1,5 +1,9 @@
 import "./ProfilePic.css";
 
 export function ProfilePic({ children }) {
-  return <div className="ProfilePic">{children.charAt(0)}</div>;
+  return (
+    <div className="ProfilePic">
+      <span className="ProfilePic-letter">{children.charAt(0)}</span>
+    </div>
+  );
 }
