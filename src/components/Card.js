b934f48fea1f754f0aea3a6cf792af0f75e563ef
Begum Utz <begum.utz@gmail.com>
2021-07-17 12:50:33 +0300

    Card layout spacing

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,10 +6,12 @@ export function Card({ name, userName, website }) {
   return (
     <div className="Card">
       <ProfilePic>{name}</ProfilePic>
-      <h3>{name}</h3>
-      <h4>@{userName}</h4>
-      <a href={website}>{website}</a>
-      <Button>More Details</Button>
+      <h3 className="Card-name">{name}</h3>
+      <h4 className="Card-username">@{userName}</h4>
+      <a className="Card-website" href={website}>
+        {website}
+      </a>
+      <Button className="Card-button">More Details</Button>
     </div>
   );
 }
