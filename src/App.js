20a27e3451e10bcafa6c345c74f466da02dde552
Begum Utz <begum.utz@gmail.com>
2021-07-18 12:48:53 +0300

    Add Home route

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <Router>
-      <Home />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
     </Router>
   );
 }
